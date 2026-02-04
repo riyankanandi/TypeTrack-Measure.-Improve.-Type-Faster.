46b40a5b03b0c52c9d6f6b854aeee1f12718384a
Riyanka Nandi <riyankanandi28207@gmail.com>
2026-02-04 11:14:48 +0530

    Connect backend to Supabase DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,13 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: "localhost",
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: 5432,
+  // host: "localhost",
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
+  // port: 5432,
+  connectionString: process.env.DB_URL,
+  ssl: { rejectUnauthorized: false },
 });
 pool.query("SELECT NOW()", (err, res) => {
   if (err) {
